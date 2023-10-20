735f5d6615e8a7a642503c8b42f4a1f99b094f30
mubashirvp7736 <vpmubashir384@gmail.com>
2023-10-20 07:01:33 -1200

    first commit

---
+++ return.dart
@@ -0,0 +1,12 @@
+
+
+
+
+//Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+
+
+
+String greet(String name) {
+  return "Hello, $name how are you doing today?";
+}
